a4a2cca4da4c456dd2575f3eb19624d291b9f9e5
yatemmma <yatemmma@gmail.com>
2015-12-13 22:25:47 +0900

    wip notify

---
+++ history.js
@@ -0,0 +1,10 @@
+var history = {
+  
+};
+history.save = function(key, value) {
+  localStorage['history#'+key] = value;
+};
+history.loadAll = function() {
+  return n * n;
+};
+module.exports = history;
--- window.js
+++ window.js
@@ -1,6 +1,7 @@
-
+var history = require('./history.js');
 var loginOptions = {
-  nickname: 'aiueo', channels: '#aiueo'
+  nickname: localStorage['setting-nickname'] || '',
+  channels: localStorage['setting-channels'] || ''
 };
 
 var webview = document.getElementById("qwebirc");
@@ -17,5 +18,47 @@ webview.addEventListener("dom-ready", function() {
 });
 
 webview.addEventListener('ipc-message', function(event) {
-  console.log(event);
+  var dateTime = new Date(event.timeStamp).toISOString();
+  var date = dateTime.split('T')[0];
+  var time = dateTime.split('T')[1].split('.')[0]
+  var obj = event.args[0];
+  var channel = obj['4'].name;
+  var message = obj['0'];
+  if (channel === 'Status') {
+    return;
+  }
+  message = message.split("").filter(function(x){
+    return x.charCodeAt(0) >= 32;
+  }).join('');
+  
+  if (message.indexOf("4==") == 0) {
+    message = message.replace(/4== qwebirc:\/\/whois\/([^\/]*)\//, '== ' + "$1");
+  } else if (message.indexOf("<") == 0) {
+    searchWord = message.replace(/15qwebirc:\/\/whois\/([^\/]*)\//, "");
+    message = message.replace(/15qwebirc:\/\/whois\/([^\/]*)\//, "$1");
+    console.log(searchWord);
+    var keywords = (localStorage['setting-keywords'] || '').split(',').map((x)=>{
+      return x.trim();
+    });
+    keywords.forEach((word)=>{
+      if (searchWord.indexOf(word) > 0) {
+        notifyKeyword(channel, message, word);
+      }
+    });
+  }
+  
+  message = '[' + time + '] ' + message;
+  
+  var key = date + channel;
+  console.log(new Date(event.timeStamp).toISOString());
+  console.log(key + ' | ' + message);
+  // history.save(key, message);
 });
+
+function notifyKeyword(channel, message, word) {
+  // alert(message);
+  var option = {
+    body: message
+  }
+  new Notification("qwebirc-client: " + channel, option);
+}
