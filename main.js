1328175edf6c098285472143a25fa96ff8d2a012
yatemmma <yatemmma@gmail.com>
2015-12-27 12:30:35 +0900

    wip

--- main.js
+++ main.js
@@ -24,11 +24,40 @@ app.on('ready', ()=>{
 
 function setMenu() {
   var template = [{
+    label: 'Edit',
+    submenu: [{
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },{
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },{
+        type: 'separator'
+      },{
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },{
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },{
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },{
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      }]
+  },{
     label: 'Setting',
     submenu: [{
         label: 'Setting',
         click: openSettingWindow
-      }, {
+      },{
         label: 'History',
         click: openHistoryWindow
       }]
